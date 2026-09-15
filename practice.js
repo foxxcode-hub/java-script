0754ce4bed039f6d3e82fd77d23025b97808a010
Himesh vaskar <hcsk.himeshv8a09@gmail.com>
2026-09-15 16:32:49 +0530

    initial

---
+++ practice.js
@@ -0,0 +1,17 @@
+const studentname = "himesh";
+let age = 19;
+let course = "BCA";
+const college = "SITM";
+var marks = "88";
+let birthyear = "2006";
+
+age = "17";
+marks = "70";
+course = "b.tech";
+
+console.log(studentname);
+console.log(age);
+console.log(course);
+console.log(college);
+console.log( marks);
+console.log(birthyear);
---
+++ problem.js
@@ -0,0 +1,15 @@
+let n = parseInt(prompt("Enter a number"));
+
+if (n % 2 == 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+
+function greet (name) 
+{
+    console.log("hello " + name );
+}
+greet ("Rahul");
+greet("priya");
+greet("parthona");
