0e7547e64a33933bd703ee16650df2974b356904
Himesh vaskar <hcsk.himeshv8a09@gmail.com>
2026-09-22 17:22:13 +0530

    studentinfo

---
+++ studentinfo.js
@@ -0,0 +1,28 @@
+let studentName = "Rahul";
+let age = 18;
+
+const course = "BCA";
+const collegeName = "ABC College";
+
+var marks = 75;
+var birthYear = 2008;
+
+let city = "Silchar";
+let semester = 1;
+let favouriteSubject = "Computer Science";
+
+age = 19;
+marks = 82;
+
+console.log("----- Student Information -----");
+
+console.log("Name:", studentName);
+console.log("Age:", age);
+console.log("Course:", course);
+console.log("College:", collegeName);
+console.log("Marks:", marks);
+console.log("Birth Year:", birthYear);
+
+console.log("City:", city);
+console.log("Semester:", semester);
+console.log("Favourite Subject:", favouriteSubject);
